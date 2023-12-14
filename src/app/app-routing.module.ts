e30dc48f1acf8d1d4f727cda957772354b33ec15
ysfadd <153532004+ysfadd@users.noreply.github.com>
2023-12-14 14:44:33 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
-import {EmailFormComponent } from './email-form/email-form.component';
-import {ContactComponent } from './contact/contact.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
